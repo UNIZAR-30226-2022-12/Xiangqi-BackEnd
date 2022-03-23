6e29321daef87f00e47cbaf365dfca2600a844af
root <root@DESKTOP-MNLGO57.localdomain>
2022-03-23 17:05:36 +0100

    v1.3

--- src/prueba.py
+++
@@ -1,8 +0,0 @@
-
-import datetime
-
-
-today = datetime.date.today()
-yesterday = today - datetime.timedelta(days=1)
-dia = datetime.datetime.strptime('2022-03-17', '%Y-%m-%d').date()
-print((yesterday- today).days)
