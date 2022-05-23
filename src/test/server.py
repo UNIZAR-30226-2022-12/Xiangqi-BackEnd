ef8114900c0ed6f5f2a2f8ad187da1f8a1c41460
Mr.Zheng <76257214+alexzheng2000@users.noreply.github.com>
2022-05-23 20:55:21 +0200

    servidor final creo

--- src/test/server.py
+++ src/test/server.py
@@ -1,7 +1,6 @@
 from aiohttp import web
-import socketio
-
 from server_helper import *
+import socketio
 
 
 ## creates a new Async Socket IO Server
@@ -34,18 +33,6 @@ async def login(sid, data):
 
     #return returnValue
     await sio.emit('returnLogin', returnValue)
-    
-@sio.on('startGame')
-async def startGame(sid, data):
-    ## When we receive a new event of type
-    ## 'message' through a socket.io connection
-    ## we print the socket ID and the message
-    print("Socket ID: " , sid)
-    print(data)
-    
-    returnValue = crearTablero();
-    #return returnValue
-    await sio.emit('returnStartGame', returnValue)
 
 @sio.on('register')
 async def register(sid, data):
@@ -62,38 +49,6 @@ async def profile(sid, data):
     returnValue = perfil(data)
 
     await sio.emit('returnProfile', returnValue)   
-
-room = {}
-
-@sio.on('sendMsg')
-async def send(sid, data):
-    print("sendMsg ", sid)
-    await sio.emit('receiveMsg', data, room=data['id'], skip_sid=sid)
-    if not data['id'] in room:
-        room[data['id']] = [sio, None] 
-    else: 
-        print(sio)
-        print(room[data['id']][0])
-        if sid == room[data['id']][0]:
-            await sio.emit('receiveMsg', data, to = room[data['id']][1]) 
-        else:
-            await sio.emit('receiveMsg', data, to = room[data['id']][0])   
-    
-@sio.on('connection')
-async def send(sid, data):
-    print("connect room ", data['id'])
-    
-    sio.enter_room(sid, data['id'])
-    
-    if data['id'] in room:
-        print("existe")
-        room[data['id']][1] = sid
-        print(sid)
-    else: 
-        print("no existe")
-        room[data['id']] = [sid, None] 
-        print(sid)
-  
 
 ## We bind our aiohttp endpoint to our app
 ## router
